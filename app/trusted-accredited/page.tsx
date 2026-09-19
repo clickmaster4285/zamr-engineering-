8bcd84d54bbf7a2cc6f29e468b2a558270871976
MalikAmirJaved <aqabjavedmalik2002@gmail.com>
2026-09-19 17:11:39 +0500

    remove unwanted

--- app/trusted-accredited/page.tsx
+++ app/trusted-accredited/page.tsx
@@ -2,16 +2,15 @@
 import TrustedAccreditedHero from "@/components/trusted-accredited/Hero";
 import IndustryCertifications from "@/components/trusted-accredited/IndustryCertifications";
 import Journey from "@/components/trusted-accredited/Journey";
-import TrustedContact from "@/components/trusted-accredited/Contact";
 import CTASection from "@/components/trusted-accredited/CTASection";
-
+import Contacts from "@/components/Contacts";
 export default function Page() {
   return (
     <main className="flex w-full flex-col items-center">
       <TrustedAccreditedHero />
       <IndustryCertifications />
       <Journey />
-      <TrustedContact />
+      <Contacts />
       <CTASection />
     </main>
   );
